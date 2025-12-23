5c106c204b2240383319ae8b49acad61bfce9963
Prashant-panchal05 <pp2575676@gmail.com>
2025-12-23 21:11:00 +0530

    changed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -62,7 +62,7 @@ io.on("connection", (socket) => {
             roomId
         });
 
-        socket.emit("room:join", {id : socket.id});
+        socket.emit("room:join", { id: socket.id });
     });
 
 
@@ -70,20 +70,29 @@ io.on("connection", (socket) => {
         io.to(to).emit("incomming:call", { from: socket.id, offer });
     })
 
-    socket.on('call:accepted',({ to , ans}) => {
-        io.to(to).emit("call:accepted", { from: socket.id, ans});
+    socket.on('call:accepted', ({ to, ans }) => {
+        io.to(to).emit("call:accepted", { from: socket.id, ans });
     })
 
-        socket.on('peer:nego:needed', ({to, offer}) => {
-        io.to(to).emit("peer:nego:needed", { from: socket.id, offer});
+    socket.on('peer:nego:needed', ({ to, offer }) => {
+        io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
     })
 
-    socket.on('peer:nego:done', ({to, ans}) => {
-        io.to(to).emit("peer:nego:final", { from: socket.id, ans});
+    socket.on('peer:nego:done', ({ to, ans }) => {
+        io.to(to).emit("peer:nego:final", { from: socket.id, ans });
     })
 });
 
 // Start serverc
-server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+const startServer = async () => {
+    try {
+        await connectToMongo();
+        server.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
+    } catch (err) {
+        console.error("Mongo connection failed:", err);
+    }
+};
+
+startServer();
