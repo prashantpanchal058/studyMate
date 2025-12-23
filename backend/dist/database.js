0f2b5f37f16552ef17746c9d8b20f0f7995b69be
Prashant-panchal05 <pp2575676@gmail.com>
2025-12-24 00:12:01 +0530

    update dist 4

--- backend/dist/database.js
+++ backend/dist/database.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const mongoURI = process.env.MONGODB_URL || "mongodb+srv://prashantpanchal058_db_user:pWKSsslkwtew2XVL@cluster0.vzkusa2.mongodb.net/?appName=Cluster0/studymate";
+const mongoURI = "mongodb+srv://prashantpanchal058_db_user:pWKSsslkwtew2XVL@cluster0.vzkusa2.mongodb.net/?appName=Cluster0/studymate";
 const connectToMongo = async () => {
     await mongoose_1.default.connect(mongoURI)
         .then(() => console.log("database is connected."));
